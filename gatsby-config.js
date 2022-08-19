9979a27f9263597b41a670986ef864cb98d22e09
drewpoling <drew@mythictech.com>
2022-08-18 23:17:26 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  siteMetadata: {
+    title: `My Gatsby Site`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "ZiSjJI4pzm5D-fkx_Y4vEHxR96SWixyxjHJ8cQZL2Ww",
+      "spaceId": "d3binx2rnqui"
+    }
+  }]
+};
